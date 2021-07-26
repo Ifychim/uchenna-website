5b3faeb7d73233e146f6beb5dd944ad1da0a6be6
Uchenna Chima <Ifychim@gmail.com>
2021-07-26 17:45:05 -0400

    finalized changes on beginner javascript fundamentals. added 2 functionalities using onClick(), picture changes. As well as a button below that lets the user change the welcome message.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,44 @@
 let uchenna_img = document.querySelector('#uchenna-picture');
 
 uchenna_img.onclick = function() {
-    let adesanya_img = uchenna_img.getAttribute('src');
+    let uchenna_img_src = uchenna_img.getAttribute('src');
 
-    if(adesanya_img === './images/uchenna-picture.jpg'){
+    if(uchenna_img_src === './images/uchenna-picture.jpg'){
         uchenna_img.setAttribute('src', './images/adesanya-img.jpg');
     }else{
         uchenna_img.setAttribute('src', './images/uchenna-picture.jpg')
     }
 
     
+}
+
+let change_usr_btn = document.querySelector('.change-user-btn');
+let website_header = document.querySelector('.name-header');
+
+function setUserName() {
+
+    let myName = prompt('Please enter your name.');
+
+    if(!myName){
+        alert("please enter your name in the text box.");
+        setUserName();
+    }else{
+        localStorage.setItem('name', myName);
+        website_header.textContent = "Welcome to Uchenna's Website, " + myName;
+    }
+
+
+
+
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem('name');
+    website_header.textContent = "Welcome to Uchenna's Website, " + storedName;
+}
+
+change_usr_btn.onclick = function() {
+    setUserName();
 }
